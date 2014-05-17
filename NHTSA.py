c71b91f26ab40752fcf19941921fec587117ef23
Sameer Adhikari <tri2sing@yahoo.com>
2014-05-17 15:48:01 -0700

    Updated API params to get details about one model after exploring various paramters

--- NHTSA.py
+++ NHTSA.py
@@ -2,8 +2,18 @@ from urllib2 import urlopen
 from json import load
 
 apiUrl = 'http://www.nhtsa.gov/webapi/api/SafetyRatings'
-apiParam = ''
+# Different levels of params to explore the data
+
+# apiParam = ''
+# apiParam = '/modelyear/2005'
+# apiParam = '/modelyear/2005/make/acura'
+# apiParam = '/modelyear/2005/make/acura/model/rsx'
+
+# Finally we get safety ratings for a specific id
+apiParam = '/vehicleid/1491'
+
 outputFormat = '?format=json'
+print apiUrl + apiParam + outputFormat
 
 response = urlopen(apiUrl + apiParam + outputFormat)
 json_obj = load(response)
